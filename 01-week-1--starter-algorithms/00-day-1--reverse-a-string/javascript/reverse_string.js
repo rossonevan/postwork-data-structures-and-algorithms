174f143c7a54d7b0bd26dfbbeaac79a4ee8e750d
Evan Rosson <rossonevan7281@gmail.com>
2023-01-16 11:23:02 -0600

    Completed Week 1 Day 1

--- 01-week-1--starter-algorithms/00-day-1--reverse-a-string/javascript/reverse_string.js
+++ 01-week-1--starter-algorithms/00-day-1--reverse-a-string/javascript/reverse_string.js
@@ -1,5 +1,16 @@
+
 function reverseString(str) {
-  // type your code here
+  // Initialize a new string output
+  let newStr = "";
+
+  // Create a for loop that will find the last letter of the input string
+  for (let i = str.length - 1; i >= 0; i--) {
+    // Add each letter iteration to the new string variable
+    newStr = newStr + str[i]
+  }
+
+  //Return the reversed string
+  return newStr;
 }
 
 if (require.main === module) {
@@ -17,3 +28,13 @@ module.exports = reverseString;
 
 // Please add your pseudocode to this file
 // And a written explanation of your solution
+
+// The function takes in a string. It then creates a new empty string
+// where the newly reversed string will be placed into. A for loop was created
+// to iterate from the end of the input string in a reverse order.
+// The loop finds the length of the string and then subtracts 1 letter from the end,
+// grabbing the last letter of the string and placing that letter into the new string variable.
+  // Ex. str = "hi", newStr =""
+    // First iteration: str[1] = "i", newStr = "" + "i" = "i"
+    // Second iteration str[0] = "h", newStr = "i" + "h" ="ih"
+// Once i = 0, the loop stops and returns the new string variable.
