9037de14e38791ec1dec0a25a695642eababafd2
Evan Rosson <rossonevan7281@gmail.com>
2023-01-17 11:39:48 -0600

    Finished Week 1 Day 2

--- 01-week-1--starter-algorithms/01-day-2--find-first-duplicate/javascript/find_first_duplicate.js
+++ 01-week-1--starter-algorithms/01-day-2--find-first-duplicate/javascript/find_first_duplicate.js
@@ -1,6 +1,24 @@
 function findFirstDuplicate(arr) {
-  // type your code here
-}
+  // If an value is repeated, return that value and end the loop
+
+    // Create a new array using the Set method to compare existing values to the input array values
+    let newArr = new Set();
+
+    // Create a for loop iterating over the length of the array
+    for (let i = 0; i < arr.length; i++) {
+      // Condition that checks if any value of the input array is contained in the Set array
+      if (newArr.has(arr[i]))
+        // If the Set array already contains the same value, return the duplicate value
+        return arr[i];
+        // If the value is not a duplicate, add the value into the Set array and continue looping through the array
+        newArr.add(arr[i]) 
+    }
+
+    // If there are no duplicates, return -1
+    return -1
+
+  }
+  
 
 if (require.main === module) {
   // add your own tests in here
@@ -11,9 +29,17 @@ if (require.main === module) {
 
   console.log("Expecting: -1");
   console.log("=>", findFirstDuplicate([1, 2, 3, 4]));
+
+  console.log("Expecting: 40")
+  console.log("=>", findFirstDuplicate([10, 20, 30, 40, 50, 40, 30, 20, 10]));
+
+  console.log("Expecting: -8")
+  console.log("=>", findFirstDuplicate([-10, -8, -4, 4, 8, 10, -1, 1, -8, -4, -10]));
 }
 
 module.exports = findFirstDuplicate;
 
 // Please add your pseudocode to this file
 // And a written explanation of your solution
+
+// Create a for loop
